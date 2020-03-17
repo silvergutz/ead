55224ca5f457a831d4034c5a826646ac2eab1b50
Tiago Vergutz <silver.yep@gmail.com>
2020-03-17 06:20:57 -0300

    chore(web): correct use of env var to config backend api url

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import auth from './auth';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 api.interceptors.request.use(async config => {

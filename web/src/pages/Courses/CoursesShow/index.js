a7fd6cb2da380f30666092fa218783339745d699
Tiago Vergutz <silver.yep@gmail.com>
2020-03-28 22:54:39 -0300

    feat(web): course page show modules with accordion

--- web/src/pages/Courses/CoursesShow/index.js
+++ web/src/pages/Courses/CoursesShow/index.js
@@ -14,6 +14,7 @@ import PageNotFound from '../../PageNotFound';
 function CoursesShow() {
   const { id } = useParams();
 
+  const [ activeModules, setActiveModules ] = useState([]);
   const [ course, setCourse ] = useState({});
   const [ modules, setModules ] = useState([]);
   const [ lesson, setLesson ] = useState({});
@@ -38,6 +39,7 @@ function CoursesShow() {
           setModules(course.modules);
           if (course.modules.length > 0 && course.modules[0].lessons.length > 0) {
             setLesson(course.modules[0].lessons[0]);
+            setActiveModules([course.modules[0].id]);
           }
         }
       }
@@ -53,6 +55,18 @@ function CoursesShow() {
 
     setVideo(lesson.video);
   }, [lesson])
+
+  function isModuleActive(id) {
+    return activeModules.indexOf(id) >= 0 ? true : false;
+  }
+
+  function toggleActiveModule(id) {
+    if (isModuleActive(id)) {
+      setActiveModules(activeModules.filter(e => e !== id));
+    } else {
+      setActiveModules([...activeModules, id]);
+    }
+  }
 
   if (course === false) {
     return (
@@ -88,13 +102,16 @@ function CoursesShow() {
               <ul>
                 {modules.map((m, i) => (
                   <li key={m.id} className="module">
-                    <div className="module-name-wraper name-wraper">
+                    <div className="module-name-wraper name-wraper" onClick={e => toggleActiveModule(m.id)}>
                       <span className="module-number number">{i+1}</span>
                       <span className="module-name name">{m.name}</span>
+                      <i className="mi toggle">
+                        {isModuleActive(m.id) ? 'arrow_drop_up' : 'arrow_drop_down'}
+                      </i>
                     </div>
 
                     {m.lessons.length > 0 &&
-                      <ul className="module-lessons">
+                      <ul className={`module-lessons${isModuleActive(m.id) ? ' active' : ''}`}>
                         {m.lessons.map((value, i) => (
                           <li key={value.id} className={`lesson name-wraper${value === lesson ? ' active' : ''}`}>
                             <span className="lesson-number number">{i+1}</span>

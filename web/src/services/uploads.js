ff99ba147c2fb167b37aae2c41b6f0f6e45d0d1a
Tiago Vergutz <silver.yep@gmail.com>
2020-04-10 18:45:37 -0300

    feat(web): allow to force download when request files

--- web/src/services/uploads.js
+++ web/src/services/uploads.js
@@ -1,14 +1,19 @@
 import api from './api';
+import querystring from 'querystring';
 
-export const download = async (file) => {
+export const download = async (file, force) => {
   try {
     // Is a full URL
     if (file.indexOf('http') === 0) {
       return file;
     }
 
+    const qsData = {};
+    if (file) qsData.file = file;
+    if (force) qsData.force = 1;
+
     // Is a partial URL that needs to pass for /downloads
-    const response = await api.get('download?file=' + file, {
+    const response = await api.get(`download?${querystring.stringify(qsData)}`, {
       responseType: 'blob'
     });
 

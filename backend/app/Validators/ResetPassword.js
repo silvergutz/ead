93941dc22c03096678fdce170ae59c378c07b16a
Tiago Vergutz <silver.yep@gmail.com>
2020-04-13 23:48:40 -0300

    fix: added required validator for password field

--- backend/app/Validators/ResetPassword.js
+++ backend/app/Validators/ResetPassword.js
@@ -5,7 +5,7 @@ const BaseValidator = use('App/Validators/BaseValidator')
 class ResetPassword extends BaseValidator {
   get rules () {
     return {
-      password: 'confirmed|min:6|max:30',
+      password: 'required|confirmed|min:6|max:30',
     }
   }
 }

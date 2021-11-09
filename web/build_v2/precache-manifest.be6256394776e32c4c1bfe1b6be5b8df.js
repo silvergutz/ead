f4eeae9f27dcd97aa4b557a0ec0645e1b4681c49
felipeoteh <felipe@agenciasimetria.com.br>
2021-11-09 15:19:07 -0300

    users deploy patrick

---
+++ web/build_v2/precache-manifest.be6256394776e32c4c1bfe1b6be5b8df.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be52f4aacea70fa3aa2f12cdd398d54a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "013b45b68ba7a3ec2f23",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "aa028e00fac704ded662",
+    "url": "/static/css/main.d9e12e7e.chunk.css"
+  },
+  {
+    "revision": "013b45b68ba7a3ec2f23",
+    "url": "/static/js/2.083619b0.chunk.js"
+  },
+  {
+    "revision": "068db432b3eec570dec7e9f3b756f87b",
+    "url": "/static/js/2.083619b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa028e00fac704ded662",
+    "url": "/static/js/main.1cf3c290.chunk.js"
+  },
+  {
+    "revision": "fe8ba76b50c6574d59c6",
+    "url": "/static/js/runtime-main.9ad2387c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
